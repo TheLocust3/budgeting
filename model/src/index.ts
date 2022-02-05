229abd16b5263a6adba3a962543a9c056e3c37a7
TheLocust3 <jake.kinsella@gmail.com>
2022-02-05 12:31:02 -0500

    Mutilate sources and move them into integrations

--- model/src/index.ts
+++ model/src/index.ts
@@ -2,6 +2,5 @@ export * as Account from "./account";
 export * as Rule from "./rule";
 export * as Transaction from "./transaction";
 export * as User from "./user";
-export * as Source from "./source";
 export * as Integration from "./integration";
 export * as Plaid from "./plaid";
--- model/src/integration.ts
+++ model/src/integration.ts
@@ -7,35 +7,35 @@ import * as types from "io-ts-types";
 import { Exception, Format } from "magic";
 
 export namespace Internal {
-  export const PlaidCredentials = iot.type({
-      _type: iot.literal("Plaid")
-    , itemId: iot.string
-    , accessToken: iot.string
-  });
+  namespace Plaid {
+    const Credentials = iot.type({
+        itemId: iot.string
+      , accessToken: iot.string
+    });
 
-  export const Credentials = PlaidCredentials;
-  export type Credentials = iot.TypeOf<typeof Credentials>;
+    const Source = iot.type({
+        name: iot.string
+      , forAccountId: iot.string
+      , createdAt: types.option(types.DateFromISOString)
+    })
 
-  export const t = iot.type({
-      name: iot.string
-    , credentials: PlaidCredentials
-  });
-
-  export type t = iot.TypeOf<typeof t>
-  export const Json = new Format.JsonFormatter(t);
-}
-
-export namespace Channel {
-  export namespace Request {
-    export namespace Create {
-      const t = iot.type({
-          userId: iot.string
-        , name: iot.string
-        , credentials: Internal.Credentials
-      });
-
-      export type t = iot.TypeOf<typeof t>
-      export const Json = new Format.JsonFormatter(t);
-    }
+    export const t = iot.type({
+        _type: iot.literal("Plaid")
+      , name: iot.string
+      , credentials: Credentials
+      , sources: iot.array(Source)
+    })
   }
+
+  namespace None {
+    export const t = iot.type({
+        _type: iot.literal("None")
+      , name: iot.string
+    })
+  }
+
+  export const t = iot.union([Plaid.t, None.t])
+  export type t = iot.TypeOf<typeof t>
+
+  export const Json = new Format.JsonFormatter(t);
 }
