9549ffbd2f978a10c1a1c46dc4b3c95294bb8eae
TheLocust3 <jake.kinsella@gmail.com>
2022-05-20 16:59:07 +0100

    Remove engine service

--- src/logic/constants.ts
+++ src/logic/constants.ts
@@ -1,9 +1,3 @@
 export const GLOBAL_ACCOUNT = "Global";
 export const PHYSICAL_ACCOUNT = "Physical";
 export const VIRTUAL_ACCOUNT = "Virtual";
-
-export const SCHEDULER_HOST = process.env.PRODUCTION ? "scheduler" : "localhost"
-export const ENGINE_HOST = process.env.PRODUCTION ? "engine" : "localhost"
-
-export const SCHEDULER_PORT = "3002"
-export const ENGINE_PORT = "3000"
