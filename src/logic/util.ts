251be2886792ce678c198b3d033b301899f057a7
TheLocust3 <jake.kinsella@gmail.com>
2023-12-31 14:50:35 -0500

    Use central auth

--- src/logic/util.ts
+++ src/logic/util.ts
@@ -1,6 +1,7 @@
 import Express from "express";
 import { Pool } from "pg";
 import { Logger } from "pino";
+import fetch from "node-fetch";
 import * as O from "fp-ts/Option";
 import * as E from "fp-ts/Either";
 import * as TE from "fp-ts/TaskEither";
@@ -15,11 +16,11 @@ import { Exception, Message, Route, Pipe } from "../magic";
 
 export namespace AuthenticationFor {
   const tryHeader = (request: Express.Request): TE.TaskEither<Exception.t, User.Internal.t> => {
-    return Firebase.verify(request.app.locals.db)(request.log)(request.app.locals.adminAuth)(String(request.header("Authorization")));
+    return Central.verify(request.app.locals.db)(request.log)(String(request.header("Authorization")));
   }
 
   const tryCookie = (request: Express.Request): TE.TaskEither<Exception.t, User.Internal.t> => {
-    return Firebase.verify(request.app.locals.db)(request.log)(request.app.locals.adminAuth)(String(request.cookies["auth-token"]));
+    return Central.verify(request.app.locals.db)(request.log)(String(request.cookies["auth-token"]));
   }
 
   export const user = async (request: Express.Request, response: Express.Response, next: Express.NextFunction) => {
@@ -71,6 +72,37 @@ export namespace Firebase {
 
             return UserResource.getOrCreate(pool)(log)({ id: decoded.uid, email: email });
           })
+        , TE.mapLeft(() => Exception.throwUnauthorized)
+      );
+    } else {
+      return TE.throwError(Exception.throwUnauthorized);
+    }
+  }
+}
+
+export namespace Central {
+  const host = process.env.CENTRAL_BASE ? process.env.CENTRAL_BASE : "http://central-server:8080/api"
+
+  const validate = (pool: Pool) => (log : Logger) => (token: string): TE.TaskEither<Exception.t, { id: string, email: string}> => {
+    return pipe(
+        Pipe.fromPromise(log)(fetch(`${host}/users/validate`, { method: "POST", body: JSON.stringify({ token }) }))
+      , TE.chain((response) => Pipe.fromPromise(log)(response.json()))
+      , TE.chain(({ user }) => {
+          if (user === undefined) {
+            return TE.throwError(Exception.throwUnauthorized);
+          } else {
+            return TE.of({ id: user.id, email: user.email });
+          }
+        })
+    );
+  }
+
+  export const verify = (pool: Pool) => (log : Logger) => (token: string): TE.TaskEither<Exception.t, User.Internal.t> => {
+    if (token !== undefined && token !== null && token !== "") {
+      return pipe(
+          token
+        , validate(pool)(log)
+        , TE.chain(({ id, email }) => UserResource.getOrCreate(pool)(log)({ id: id, email: email }))
         , TE.mapLeft(() => Exception.throwUnauthorized)
       );
     } else {
--- src/scripts/signin.ts
+++ src/scripts/signin.ts
@@ -1,14 +1,16 @@
 import * as readline from 'readline';
 
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+const host = process.env.CENTRAL_BASE ? process.env.CENTRAL_BASE : "https://central.localhost/api"
+
+/*import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";*/
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const firebase = initializeApp({
+/*const firebase = initializeApp({
   apiKey: "AIzaSyA0TAsZg2lUNZh_GFBUDsLS5ygbEuYfGUc",
   authDomain: "budgeting-6f7c7.firebaseapp.com",
   projectId: "budgeting-6f7c7",
@@ -16,12 +18,12 @@ const firebase = initializeApp({
   messagingSenderId: "311510054173",
   appId: "1:311510054173:web:c2ee289e440c4fb5c0ddf4",
   measurementId: "G-4Q7FGZ3N7L"
-});
+});*/
 
 rl.question("email: ", (email) => {
   rl.question("password: ", async (password) => {
-    const res = await signInWithEmailAndPassword(getAuth(), email, password);
-    const token = await res.user.getIdToken(true);
+    const res = await fetch(`${host}/users/login`, { method: "POST", body: JSON.stringify({ email, password }) })
+    const token = await res.json();
     console.log(token);
 
     rl.close();
